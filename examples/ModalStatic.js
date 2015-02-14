15f1778fff318b1be7969f13c4fc8eec393630e6
Matthew Smith <mtscout6@gmail.com>
2015-02-14 14:44:45 -0700

    "Release v1.0.25"

---
+++ examples/ModalStatic.js
@@ -0,0 +1,23 @@
+function handleHide() {
+  alert('Close me!')
+}
+
+var modalInstance = (
+    <div className="static-modal">
+      <Modal title="Modal title"
+        backdrop={false}
+        animation={false}
+        container={mountNode}
+        onRequestHide={handleHide}>
+        <div className="modal-body">
+          One fine body...
+        </div>
+        <div className="modal-footer">
+          <Button>Close</Button>
+          <Button bsStyle="primary">Save changes</Button>
+        </div>
+      </Modal>
+    </div>
+  );
+
+React.render(modalInstance, mountNode);
